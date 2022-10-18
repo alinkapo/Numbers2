4916fbb1e6de2d4cf8da1491165fc4bb25441334
Андрей <@DESKTOP-USER>
2022-10-18 18:27:50 +0400

    Добавьте файлы проекта.

---
+++ Numbers2/Program.cs
@@ -0,0 +1,20 @@
+﻿Random rnd = new Random();
+int number = 20;
+int a = 1;
+int b = 900;
+
+int[] A = new int[number];
+int[] B = new int[number];
+
+for (int i = 0; i < A.Length; i++)
+{  
+   A[i] = rnd.Next(a,b);
+   if (A[i] <= 888)
+   {
+    B[i] = A[i];
+   }
+}
+Array.Sort(B);
+Array.Reverse(B);
+Console.WriteLine(string.Join("\n \b", B));
+Console.ReadLine();
